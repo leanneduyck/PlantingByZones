d7dbe3912f6adfe60b35183081bb24905ad22699
Leanne <154445685+leanneduyck@users.noreply.github.com>
2024-02-08 08:30:46 -0800

    switched content to plants
    changed all text
    added basic css
    added for loop to js array
    added conditional w/in loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-alert("Pokedex");
+alert("What is your planting zone?");
 
-let pokemonList = [
-  { name: "Charmander", height: 0.6, type: "fire" },
-  { name: "Pikachu", height: 0.4, type: "electric" },
-  { name: "Squirtle", height: 0.5, type: "water" },
+let peaList = [
+  { name: "Zone 4", date: 4.15, types: ["vegetable"] },
+  { name: "Zone 5", date: 4.1, types: ["vegetable"] },
+  { name: "Zone 6", date: 3.15, types: ["vegetable"] },
+  { name: "Zone 7", date: 3.1, types: ["vegetable"] },
+  { name: "Zone 8", date: 2.15, types: ["vegetable"] },
 ];
+
+for (let i = 0; i < peaList.length; i++) {
+  document.write(
+    peaList[i].name + " (plant date: " + peaList[i].date + ")" + "<br>"
+  );
+}
+if (peaList[date] < 3) {
+  document.write("wow, that's early!");
+}
+if (peaList.date > 4) {
+  document.write("wow, that's late!");
+}
