59d99fb90f5cf767ba0edc165a20d47852349979
Leanne <154445685+leanneduyck@users.noreply.github.com>
2024-02-16 12:30:58 -0800

    changed to addListItem(), added/styled buttons and querySelector()

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,55 @@
 prompt("What is your planting zone?");
 
-let peaList = [
-  { name: "Zone 4", date: "2024-04-15", types: ["vegetable"] },
-  { name: "Zone 5", date: "2024-04-01", types: ["vegetable"] },
-  { name: "Zone 6", date: "2024-03-15", types: ["vegetable"] },
-  { name: "Zone 7", date: "2024-03-01", types: ["vegetable"] },
-  { name: "Zone 8", date: "2024-02-15", types: ["vegetable"] },
-];
+let peaRepository = (function () {
+  let repository = [
+    { name: "Zone 4", date: "2024-04-15", types: ["vegetable"] },
+    { name: "Zone 5", date: "2024-04-01", types: ["vegetable"] },
+    { name: "Zone 6", date: "2024-03-15", types: ["vegetable"] },
+    { name: "Zone 7", date: "2024-03-01", types: ["vegetable"] },
+    { name: "Zone 8", date: "2024-02-15", types: ["vegetable"] },
+  ];
 
-letPeaRepository = (function () {
-  let peaList = [];
+  function add(respository) {
+    if (
+      tyepof(repository) === "object" &&
+      "name" in repository &&
+      "date" in repository &&
+      "types" in repository
+    ) {
+      repository.push(repository);
+    } else {
+      console.log("zone is not correct");
+    }
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  function addListItem(repository) {
+    let peaRepository = document.querySelector(".pea-list");
+    let listpea = document.createElement("li");
+    let button = document.createElement("button");
+    button.addEventListener("click", function showDetails(respository) {});
+    button.innerText = repository.name;
+    button.classList.add("button-class");
+    listpea.appendChild(button);
+    peaRepository.appendChild(listpea);
+  }
+
+  function showDetails(repository) {
+    console.log(repository.date);
+  }
 
   return {
-    add: function (peaList) {
-      peaList.push(peaList);
-    },
-    getAll: function () {
-      return peaList;
-    },
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
-function myLoopFunction(peaList) {
-  document.write(
-    "<p>" +
-      peaList.name +
-      " should be planted on " +
-      peaList.date +
-      "." +
-      "</p>"
-  );
+console.log(peaRepository.getAll());
 
-  if (peaList.date < "2024-03-15") {
-    document.write("Wow, that's early!");
-  } else if (peaList.date > "2024-03-15") {
-    document.write("Wow, that's late!");
-  }
-}
-
-peaList.forEach(myLoopFunction);
+peaRepository.getAll().forEach(function (repository) {
+  peaRepository.addListItem(repository);
+});
