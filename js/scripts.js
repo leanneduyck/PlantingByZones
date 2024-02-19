2ac6f33ba385da1fdc0b5ecb5aa5d1034bb90bc6
Leanne <154445685+leanneduyck@users.noreply.github.com>
2024-02-19 12:41:58 -0800

    attempted to replaced static list with API

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,100 @@
 prompt("What is your planting zone?");
 
-let peaRepository = (function () {
-  let repository = [
-    { name: "Zone 4", date: "2024-04-15", types: ["vegetable"] },
-    { name: "Zone 5", date: "2024-04-01", types: ["vegetable"] },
-    { name: "Zone 6", date: "2024-03-15", types: ["vegetable"] },
-    { name: "Zone 7", date: "2024-03-01", types: ["vegetable"] },
-    { name: "Zone 8", date: "2024-02-15", types: ["vegetable"] },
-  ];
+let plantRepository = (function () {
+  let plantList = [];
+  let apiUrl =
+    "https://perenual.com/api/species-list?key=sk-5nx665d38e92daa284244";
+  let showLoadingMessage = "imgloading.gif";
 
-  function add(respository) {
-    if (
-      tyepof(repository) === "object" &&
-      "name" in repository &&
-      "date" in repository &&
-      "types" in repository
-    ) {
-      repository.push(repository);
+  function add(plant) {
+    if (tyepof(plant) === "object" && "other_name" in plant) {
+      plantList.push(plant);
     } else {
-      console.log("zone is not correct");
+      console.log("information is unavailable");
     }
   }
 
   function getAll() {
-    return repository;
+    return plantList;
   }
 
-  function addListItem(repository) {
-    let peaRepository = document.querySelector(".pea-list");
-    let listpea = document.createElement("li");
+  function addListItem(plant) {
+    let plantList = document.querySelector(".plant-list");
+    let listplant = document.createElement("li");
     let button = document.createElement("button");
-    button.addEventListener("click", function showDetails(respository) {});
-    button.innerText = repository.name;
+    button.innerText = plant.common_name;
     button.classList.add("button-class");
-    listpea.appendChild(button);
-    peaRepository.appendChild(listpea);
+    listplant.appendChild(button);
+    plantList.appendChild(listplant);
+    button.addEventListener("click", function (event) {
+      showDetails(plant);
+    });
   }
 
-  function showDetails(repository) {
-    console.log(repository.date);
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        hideLoadingMessage();
+        json.data.forEach(function (id) {
+          let plant = {
+            name: id.other_name,
+            image: id.default_image,
+          };
+          add(plant);
+          console.log(plant);
+        });
+      })
+      .catch(function (e) {
+        hideLoadingMessage();
+        console.error(e);
+      });
+  }
+
+  function loadDetails(id) {
+    showLoadingMessage();
+    let url = id.default_image;
+    return fetch(url)
+      .then(function (response) {
+        hideLoadingMessage();
+        return response.json();
+      })
+      .then(function (id) {
+        hideLoadingMessage();
+        id.default_image = id.default_image;
+        id.otherName = id.other_name;
+        id.scientificName = id.scientific_name;
+        id.cycle = id.cycle;
+        id.watering = id.watering;
+        id.sunglight = id.sunlight;
+      })
+      .catch(function (e) {
+        hideLoadingMessage();
+        console.error(e);
+      });
+  }
+
+  function showDetails(plant) {
+    plantRepository.loadDetails(plant).then(function () {
+      console.log(plant);
+    });
   }
 
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
   };
 })();
 
-console.log(peaRepository.getAll());
-
-peaRepository.getAll().forEach(function (repository) {
-  peaRepository.addListItem(repository);
+plantRepository.loadList().then(function () {
+  plantRepository.getAll().forEach(function (plant) {
+    plantRepository.addListItem(plant);
+  });
 });
